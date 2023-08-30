cf5f0082522e79e1a4ca959392f1eac2a6c8d31e
qwikk <44299295+qwakk@users.noreply.github.com>
2023-08-30 17:36:46 +0200

    temp fix for accessing token.txt in resources from main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
+import commands.botcommands.Ping
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import service.F1DataService
 
 fun main() {
-    val token = Any().javaClass.getResource("token/token.txt")!!.readText()
+    val token = Ping("","").javaClass.getResource("/token/token.txt")!!.readText()
 
     val bot = JDABuilder.createDefault(token)
         .setActivity(Activity.listening("F1 theme song"))
