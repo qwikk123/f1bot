5a86279d68a2eedd98c757c475ab2fc337575d3a
qwikk <44299295+qwakk@users.noreply.github.com>
2023-08-30 14:46:33 +0200

    updated build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,20 @@ kotlin {
 application {
     mainClass.set("MainKt")
 }
+
+tasks {
+    val botJar = register<Jar>("botJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(botJar)
+    }
+}
