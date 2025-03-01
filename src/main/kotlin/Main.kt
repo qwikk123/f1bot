489c5f9a0b4d51de900f96948b8b5b3ea98e65f8
qwikk123 <44299295+qwikk123@users.noreply.github.com>
2025-03-01 11:59:46 +0100

    token fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
-import commands.botcommands.Ping
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import service.F1DataService
 
 fun main() {
-    val token = Ping("","").javaClass.getResourceAsStream("/token/token.txt")!!
-        .bufferedReader().use { it.readLine() }
+    val token = System.getenv("APP_TOKEN") ?: throw IllegalStateException("APP_TOKEN environment variable is not set")
 
     val bot = JDABuilder.createDefault(token)
         .setActivity(Activity.listening("F1 theme song"))
